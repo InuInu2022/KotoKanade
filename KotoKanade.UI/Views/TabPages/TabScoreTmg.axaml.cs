b911c1d3db70584da47bf73aea63c4e209b7319e
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-01-07 13:09:58 +0900

    chore: separate tab page files

---
+++ KotoKanade.UI/Views/TabPages/TabScoreTmg.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KotoKanade.Views;
+
+public partial class TabScoreTmg : UserControl
+{
+    public TabScoreTmg()
+    {
+        InitializeComponent();
+    }
+}
---
+++ KotoKanade.UI/Views/TabPages/TabSetting.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KotoKanade.Views;
+
+public partial class TabSetting : UserControl
+{
+    public TabSetting()
+    {
+        InitializeComponent();
+    }
+}
