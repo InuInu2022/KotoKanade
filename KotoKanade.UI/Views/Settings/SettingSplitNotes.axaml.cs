cf24f08b80d5366d1443e8fbab6d91a11b653e20
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-01-07 14:34:19 +0900

    feat: separate setting files

---
+++ KotoKanade.UI/Views/Settings/SettingSplitNotes.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KotoKanade.Views;
+
+public partial class SettingSplitNotes : UserControl
+{
+    public SettingSplitNotes()
+    {
+        InitializeComponent();
+    }
+}
