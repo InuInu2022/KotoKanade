a1fb7d49be178f5f5407a04a668029e57757d253
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-01-09 22:56:21 +0900

    test: add Core tests

---
+++ test/Core/TalkDataConvertTests.cs
@@ -0,0 +1,52 @@
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using KotoKanade.Core.Models;
+using LibSasara.Model;
+
+namespace Core;
+
+public class TalkDataConvertTests
+{
+	[ModuleInitializer]
+	[SuppressMessage("","xUnit1013")]
+	[SuppressMessage("","S3168")]
+	[SuppressMessage("","VSTHRD100")]
+    public static async void Initialize()
+    {
+		var method = typeof(TalkDataConverter).GetMethod("InitOpenJTalkAsync", BindingFlags.NonPublic | BindingFlags.Static);
+		await (dynamic?)method!
+			.Invoke("InitOpenJTalkAsync", null)
+			;
+    }
+
+
+	[Theory]
+	[InlineData(new string[]{"あーっ"}, new string[]{"あーっ"})]
+	[InlineData(new string[]{"ち","きゅ","ー","は"}, new string[]{"ち","きゅ","ウ","は"})]
+	[InlineData(new string[]{"ち","きゅ","ーは"}, new string[]{"ち","きゅ","ウは"})]
+    public void Test1(string[] lyricsArray, string[] expectLyrics)
+    {
+		List<Note> target = lyricsArray
+			.Select(n => new Note() { Lyric = n })
+			.ToList();
+		var result = RefManageLongVowelSymbols(target);
+
+		Assert.True(result?.Select(n => n.Lyric).SequenceEqual(expectLyrics, StringComparer.Ordinal));
+
+		static List<Note>? RefManageLongVowelSymbols(List<Note> p)
+		{
+			var method = typeof(TalkDataConverter).GetMethod("ManageLongVowelSymbols", BindingFlags.NonPublic | BindingFlags.Static) ?? throw new DataException();
+			try
+			{
+				var ret = method.Invoke("ManageLongVowelSymbols", [p]) as List<Note>;
+                return ret ?? default;
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException ?? new InvalidDataException(ex.Message);
+            }
+		}
+    }
+}
