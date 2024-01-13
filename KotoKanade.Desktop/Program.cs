a95d3eb2a55343e065accf4044fc37a007728eb3
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-01-13 19:57:02 +0900

    feat: add logging

--- KotoKanade.Desktop/Program.cs
+++ KotoKanade.Desktop/Program.cs
@@ -4,11 +4,17 @@
 
 using System;
 using Avalonia;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace KotoKanade;
 
 public static class Program
 {
+	private static readonly NLog.Logger Logger
+		= NLog.LogManager.GetCurrentClassLogger();
+
 	// Avalonia configuration, don't remove; also used by visual designer.
 	public static AppBuilder BuildAvaloniaApp()
 	{
@@ -21,7 +27,40 @@ public static class Program
 	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
 	// yet and stuff might break.
 	[STAThread]
-    public static int Main(string[] args) =>
-        BuildAvaloniaApp().
-        StartWithClassicDesktopLifetime(args);
+	public static int Main(string[] args)
+	{
+		try
+		{
+			InitLogger();
+			Logger.Info("App starting...");
+		  	return BuildAvaloniaApp().
+				StartWithClassicDesktopLifetime(args);
+		}
+		catch (Exception ex)
+		{
+			Logger.Fatal(ex, "Main App Error!");
+			return 1;
+		}
+		finally
+		{
+			Logger.Info("App finising...");
+		}
+	}
+
+	private static void InitLogger()
+	{
+		var config = new LoggingConfiguration();
+
+		var fileTarget = new FileTarget();
+		config.AddTarget("file", fileTarget);
+
+		fileTarget.Name = "f";
+		fileTarget.FileName = "${basedir}/logs/${shortdate}.log";
+		fileTarget.Layout = "${longdate} [${uppercase:${level}}] ${message}";
+
+		var rule1 = new LoggingRule("*", LogLevel.Info, fileTarget);
+		config.LoggingRules.Add(rule1);
+
+		LogManager.Configuration = config;
+	}
 }
