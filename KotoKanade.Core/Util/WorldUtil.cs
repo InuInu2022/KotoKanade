735cba2e5f165953c0fa0350b90c01ec96f6a01b
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-03-02 13:35:43 +0900

    feat: optimize #15

--- KotoKanade.Core/Util/WorldUtil.cs
+++ KotoKanade.Core/Util/WorldUtil.cs
@@ -1,7 +1,8 @@
-using System;
-using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using DotnetWorld.API;
 using DotnetWorld.API.Structs;
+using WavData = (int SampleRate, int nbit, int len, double[] x);
 
 namespace KotoKanade.Core.Models;
 
@@ -10,60 +11,127 @@ public static class WorldUtil
 	public static async ValueTask<WorldParam> EstimateF0Async(
 		double[] x,
 		int audioLength,
-		WorldParam wParam
+		WorldParam wParam,
+		int nbit = 16
 	)
 	{
+		var sw = new Stopwatch();
+		sw.Start();
+
 		var opt = new HarvestOption();
 		DotnetWorld.API.Core.InitializeHarvestOption(opt);
 		opt.frame_period = wParam.FramePeriod;
-		opt.f0_floor = 90.0;    //声の周波数の下のライン
+		opt.f0_floor = 90.0; //声の周波数の下のライン
+
+		sw.Stop();
+		Console.WriteLine($"[time]Estimate init {sw.Elapsed.TotalSeconds}");
+		sw.Restart();
 
 		wParam.F0Length = await Task.Run(() =>
-			DotnetWorld.API.Core.GetSamplesForDIO(
-				wParam.Fs,
-				audioLength,
-				wParam.FramePeriod
-			))
+				DotnetWorld.API.Core.GetSamplesForDIO(
+					wParam.SampleRate,
+					audioLength,
+					wParam.FramePeriod
+				)
+			)
 			.ConfigureAwait(false);
 		wParam.F0 = new double[wParam.F0Length];
 		wParam.TimeAxis = new double[wParam.F0Length];
 
-		await Task.Run(() =>
-		{
-			try
-			{
-				DotnetWorld.API.Core.Harvest(
-					x,
-					audioLength,
-					wParam.Fs,
-					opt,
-					wParam.TimeAxis,
-					wParam.F0
-				);
-			}
-			catch (System.Exception e)
-			{
-				throw new InvalidOperationException($"Estimate f0 Error: {e.Message}");
-			}
-		})
-		.ConfigureAwait(false);
+		sw.Stop();
+		Console.WriteLine($"[time]GetSamplesForDIO {sw.Elapsed.TotalSeconds}");
+		sw.Restart();
+
+		var seps = SeparateWavData((wParam.SampleRate, nbit, audioLength, x));
+		var wParams = seps
+			.AsParallel().AsOrdered()
+			.Select(v => EstimateCore(v, opt))
+			.ToList();
+		var timeAxis = wParams
+			.Select(v => v.TimeAxis)
+			.SelectMany(arr => arr ?? []).ToArray();
+		var f0 = wParams
+			.Select(v => v.F0)
+			.SelectMany(arr => arr ?? []).ToArray();
+		wParam.TimeAxis = timeAxis;
+		wParam.F0 = f0;
+
+		sw.Stop();
+		Console.WriteLine($"[time]Harvest {sw.Elapsed.TotalSeconds}");
+		sw.Restart();
 
 		return wParam;
+	}
+
+	private static WorldParam EstimateCore(
+		(int SampleRate, int nbit, int len, double[] x) wavData,
+		HarvestOption opt
+	){
+		var sepParam = new WorldParam(wavData.SampleRate);
+		sepParam.F0Length = DotnetWorld.API.Core.GetSamplesForDIO(
+			wavData.SampleRate,
+			wavData.len,
+			sepParam.FramePeriod
+		);
+		sepParam.F0 = new double[sepParam.F0Length];
+		sepParam.TimeAxis = new double[sepParam.F0Length];
+		try
+		{
+			DotnetWorld.API.Core
+				.Harvest(wavData.x, wavData.len, wavData.SampleRate, opt, sepParam.TimeAxis, sepParam.F0);
+		}
+		catch (System.Exception e)
+		{
+			throw new InvalidOperationException($"Estimate f0 Error: {e.Message}");
+		}
+		return sepParam;
+	}
+
+	public static ImmutableArray<WavData> SeparateWavData(WavData wavData)
+	{
+		//fix param
+		int retNBit = wavData.nbit;
+		int retSampleRate = wavData.SampleRate;
+
+		int separateCount = Environment.ProcessorCount;
+		int sepLen = wavData.len / separateCount;
+		//int modLen = wavData.len % separateCount;
+
+		ReadOnlySpan<double> span = wavData.x;
+		var builder = ImmutableArray.CreateBuilder<WavData>(separateCount + 1);
+		var from = 0;
+		for (var i = 0; i < separateCount; i++)
+		{
+			var to = from + sepLen;
+			Debug.Assert(from <= wavData.len);
+			Debug.Assert(to <= span.Length);
+			var sliced = span[from..to].ToArray();
+			builder.Add((retSampleRate, retNBit, sliced.Length, sliced));
+			from = to;
+		}
+		var last = span[from..].ToArray();
+		if (last.Length > 0)
+		{
+			builder.Add((retSampleRate, retNBit, last.Length, last));
+		}
+		return builder.ToImmutable();
 	}
 
 	/// <summary>
 	/// Read wav file
 	/// </summary>
-	public static async ValueTask<(int fs, int nbit, int len, double[] x)> ReadWavAsync(string filename)
+	public static async ValueTask<WavData> ReadWavAsync(string filename)
 	{
 		var isExist = System.IO.File.Exists(filename);
-		if (!isExist) {
+		if (!isExist)
+		{
 			const string msg = "A internal wav file is not found.";
 			throw new ArgumentException(msg, nameof(filename));
 		}
 
 		var audioLength = Tools.GetAudioLength(filename);
-		if(audioLength<0){
+		if (audioLength < 0)
+		{
 			//TODO: xplat chunk reader
 			throw new InvalidDataException($"{filename} has invalid data!");
 		}
@@ -71,18 +139,18 @@ public static class WorldUtil
 		var x = new double[audioLength];
 
 		return await Task.Run(() =>
-		{
-			Tools.WavRead(filename, out int fs, out int nbit, x);
-			return (fs, nbit, audioLength, x);
-		})
-		.ConfigureAwait(false);
+			{
+				Tools.WavRead(filename, out int fs, out int nbit, x);
+				return (fs, nbit, audioLength, x);
+			})
+			.ConfigureAwait(false);
 	}
 }
 
 public record WorldParam
 {
 	public double FramePeriod { get; set; }
-	public int Fs { get; set; }
+	public int SampleRate { get; set; }
 
 	public double[]? F0 { get; set; }
 	public double[]? TimeAxis { get; set; }
@@ -92,9 +160,9 @@ public record WorldParam
 	public double[,]? Aperiodicity { get; set; }
 	public int FFTSize { get; set; }
 
-	public WorldParam(int fs, double framePeriod = 5.0)
+	public WorldParam(int sampleRate, double framePeriod = 5.0)
 	{
-		Fs = fs;
+		SampleRate = sampleRate;
 		FramePeriod = framePeriod;
 	}
 }
