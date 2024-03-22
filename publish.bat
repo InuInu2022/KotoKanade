017da02c133e9d39e062a934c46a1a9ac74de5c8
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-03-23 03:15:52 +0900

    chore

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
-dotnet publish -c Release -r win-x64
-dotnet publish -c Release -r osx-x64
-dotnet publish -c Release -r osx-arm64
-dotnet publish -c Release -r linux-x64
+dotnet publish -c Release -r win-x64 -f net8.0
+dotnet publish -c Release -r osx-x64 -f net8.0
+dotnet publish -c Release -r osx-arm64 -f net8.0
+dotnet publish -c Release -r linux-x64 -f net8.0
